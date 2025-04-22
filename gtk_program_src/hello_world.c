758cbd34de787a8874660a8301e2cae88bb4ff4d
BusterLundgaard <bbcool@hotmail.dk>
2025-04-22 16:46:50 +0200

    Small shit

--- gtk_program_src/hello_world.c
+++ gtk_program_src/hello_world.c
@@ -263,7 +263,6 @@ main (int    argc,
 {
     GtkApplication *app;
     int status;
-
     app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
     status = g_application_run (G_APPLICATION (app), argc, argv);
