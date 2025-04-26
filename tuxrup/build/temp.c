6efe8f64af69bd19daff4ba1757db7936ae21254
Buster <au721321@uni.au.dk>
2025-04-26 11:14:12 +0200

    Changed the gitignore files so we track more things in build

---
+++ tuxrup/build/temp.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define stupid_ass_macro char**
+struct {
+  int wow;
+  long int more_wow;
+};
+typedef struct {
+  int wow;
+  long int more_wow;
+} my_epic_struct;
+typedef int my_own_int;
+ 
+ void customfunction(GtkWidget *widget, gpointer   data){
+ //PREFIX: 
+void** _data = (void**)data; 
+    typedef void(*some_original_function_t)(int);
+    some_original_function_t some_original_function = (some_original_function_t)(_data[1]);
+    typedef int some_original_variable_t;
+    some_original_variable_t some_original_variable = *((some_original_variable_t*)(_data[0]));
+
+ //THEIR FUNCTION: 
+    some_original_variable--;
+    my_own_int local_var_fuck_you = some_original_variable;;
+    ;;
+    some_original_function(local_var_fuck_you);
+
+//POSTFIX: 
+    *((some_original_variable_t*)(_data[0])) = some_original_variable;
+ 
+}
